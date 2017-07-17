170c78af0aff11c7523f3916f983dc058e248c11
Angelo <angeloparziale94@gmail.com>
2017-07-17 16:36:35 +0200

    new utils for section call of papers

--- utils/commiteeMembers.php
+++ utils/commiteeMembers.php
@@ -1,7 +1,262 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: angelo
- * Date: 15/07/17
- * Time: 11.57
- */
+<!-- FIRSTO ROW -->
+<div class="row">
+    <div class="col-md-4">
+        <a href="http://mcis.polymtl.ca/bram.html" target="_blank">Bram Adams</a>
+        <p>Ecole Polytechnique De Montreal, Canada</p>
+    </div>
+    <div class="col-md-4">
+        <a href="http://www.antoniol.net" target="_blank">Giuliano Antoniol</a>
+        <p>Ecole Polytechnique De Montreal, Canada</p>
+    </div>
+    <div class="col-md-4">
+        <a href="https://school.eecs.wsu.edu/people/faculty/venera-arnaoudova/" target="_blank">Venera Arnaoudova</a>
+        <p>Washington State University, USA</p>
+    </div>
+</div>
+<!-- SECONDA RIGA -->
+<div class="row">
+    <div class="col-md-4">
+        <a href="http://www.inf.usi.ch/faculty/bavota/" target="_blank">Gabriele Bavota</a>
+        <p>Universit&agrave della Svizzera Italiana, Switzerland</p>
+    </div>
+    <div class="col-md-4">
+        <a href="http://www.cs.loyola.edu/~binkley/" target="_blank">Dave Binkley</a>
+        <p>Loyola University Maryland, USA</p>
+    </div>
+    <div class="col-md-4">
+        <a href="http://research.microsoft.com/en-us/people/cbird/" target="_blank">Christian Bird</a>
+        <p>Microsoft Research, USA</p>
+    </div>
+</div>
+<!-- TERZA RIGA -->
+<div class="row">
+    <div class="col-md-4">
+        <a>Magiel Bruntink</a>
+        <p>Software Improvement Group, The Netherlands</p>
+    </div>
+    <div class="col-md-4">
+        <a href="http://selab.fbk.eu/ceccato/" target="_blank">Mariano Ceccato</a>
+        <p>Fondazione Bruno Kessler, Italy</p>
+    </div>
+    <div class="col-md-4">
+        <a href="https://directory.unamur.be/staff/acleve" target="_blank">Anthony Cleve</a>
+        <p>University of Namur, Belgium</p>
+    </div>
+</div>
+<!-- QUARTA RIGA -->
+<div class="row">
+    <div class="col-md-4">
+        <a href="http://mlcollard.net" target="_blank">Michael Collard</a>
+        <p>The University of Akron, USA</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="http://damevski.github.io">Kostadin Damevski</a>
+        <p>Virginia Commonwealth University, USA</p>
+    </div>
+    <div class="col-md-4">
+        <a href="http://docenti.unisa.it/003241/home" target="_blank">Andrea De Lucia</a>
+        <p>University of Salerno, Italy</p>
+    </div>
+</div>
+<!-- QUINTA RIGA -->
+<div class="row">
+    <div class="col-md-4">
+        <a href="http://win.ua.ac.be/~sdemey/" target="_blank">Serge Demeyer</a>
+        <p>Universiteit Antwerpen, Belgium</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="http://www.sed.inf.u-szeged.hu/member/rudolf-ferenc-phd">Rudolf Ferenc</a>
+        <p>University of Szeged, Hungary</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="https://plg.uwaterloo.ca/~migod/">Mike Godfrey</a>
+        <p>University of Waterloo, Canada</p>
+    </div>
+</div>
+<!-- SESTA RIGA -->
+<div class="row">
+    <div class="col-md-4">
+        <a target="_blank" href="http://www.gousios.gr">Georgios Gousios</a>
+        <p>Delft University of Technology, The Netherlands</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="http://docenti.unisa.it/004724/home">Carmine Gravino</a>
+        <p>University of Salerno, Italy</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="http://gyimothy.sed.hu">Tibor Gyimothy</a>
+        <p>University of Szeged, Hungary</p>
+    </div>
+</div>
+<!-- SETTIMA RIGA -->
+<div class="row">
+    <div class="col-md-4">
+        <a target="_blank" href="https://www.cs.fsu.edu/~shaiduc/">Sonia Haiduc</a>
+        <p>Florida State University, USA</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="http://www.se.cs.titech.ac.jp/~hayashi/">Shinpei Hayashi</a>
+        <p>Tokyo Institute of Technology, Japan</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="http://akinori-ihara.jpn.org">Akinori Ihara</a>
+        <p>Nara Institute of Science and Technology, Japan</p>
+    </div>
+</div>
+<!-- OTTAVA RIGA -->
+<div class="row">
+    <div class="col-md-4">
+        <a target="_blank" href="http://sel.ist.osaka-u.ac.jp/people/inoue/">Katsuro Inoue</a>
+        <p>Osaka University, Japan</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="http://posl.ait.kyushu-u.ac.jp/~kamei/">Yasutaka Kamei</a>
+        <p>Kyushu University, Japan</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="http://www.khomh.net">Foutse Khomh</a>
+        <p>École Polytechnique de Montréal, Canada</p>
+    </div>
+</div>
+<!-- NONA RIGA -->
+<div class="row">
+    <div class="col-md-4">
+        <a>Dongsun Kim</a>
+        <p>Sogang University, South Korea</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="http://blog.iese.fraunhofer.de/author/jens-knodel/">Jens Knodel</a>
+        <p>Fraunhofer IESE, Germany</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="http://www.sa.cs.titech.ac.jp/~tkobaya/">Takashi Kobayashi</a>
+        <p>Tokyo Institute of Technology, Japan</p>
+    </div>
+</div>
+<!-- DECIMA RIGA -->
+<div class="row">
+    <div class="col-md-4">
+        <a target="_blank" href="http://www.informatik.uni-bremen.de/~koschke/">Rainer Koschke</a>
+        <p>University of Bremen, Germany</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="http://nicholasakraft.com">Nicholas A. Kraft</a>
+        <p>ABB Corporate Research, Switzerland</p>
+    </div>
+    <div class="col-md-4">
+        <a>Lucas Layman</a>
+        <p>Fraunhofer CESE, College Park, USA</p>
+    </div>
+</div>
+<!-- UNDICESIMA RIGA -->
+<div class="row">
+    <div class="col-md-4">
+        <a target="_blank" href="https://mircealungu.github.io">Mircea Lungu</a>
+        <p>University of Bern, Switzerland</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="http://staff.cs.upt.ro/~cristina/">Cristina Marinescu</a>
+        <p>Politehnica University Timisoara, Romania</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="http://shanemcintosh.org">Shane Mcintosh</a>
+        <p>McGill University, Canada</p>
+    </div>
+</div>
+<!-- DODICESIMA RIGA -->
+<div class="row">
+    <div class="col-md-4">
+        <a target="_blank" href="http://www3.nd.edu/~cmc/">Collin McMillan</a>
+        <p>University of Notre Dame, USA</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="http://www.inf.usi.ch/postdoc/mocci/">Andrea Mocci</a>
+        <p>Università della Svizzera Italiana, Switzerland</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="https://www.cs.colostate.edu/~lmorenoc/">Laura Moreno</a>
+        <p>Colorado State University, USA</p>
+    </div>
+</div>
+<!-- 13° RIGA -->
+<div class="row">
+    <div class="col-md-4">
+        <a target="_blank" href="http://www.se.rit.edu/~mei/">Meiyappan Nagappan</a>
+        <p>Rochester Institute of Technology, USA</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="http://scg.unibe.ch/staff/oscar">Oscar Nierstrasz</a>
+        <p>University of Bern, Switzerland</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="http://www.ifi.uzh.ch/en/seal/people/panichella.html">Sebastiano Panichella</a>
+        <p>University of Zurich, Switzerland</p>
+    </div>
+</div>
+<!-- 14° RIGA -->
+<div class="row">
+    <div class="col-md-4">
+        <a target="_blank" href="http://www.se.fudan.edu.cn/pengxin/">Xin Peng</a>
+        <p>Fudan University, China</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="https://serg.aau.at/bin/view/MartinPinzger/WebHome">Martin Pinzger</a>
+        <p>University of Klagenfurt, Austria</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="http://www.cs.wm.edu/~denys/">Denys Poshyvanyk</a>
+        <p>College of William and Mary, USA</p>
+    </div>
+</div>
+<!-- 15° RIGA -->
+<div class="row">
+    <div class="col-md-4">
+        <a target="_blank" href="https://www.inf.unibz.it/~rrobbes/">Romain	Robbes</a>
+        <p>Free University of Bolzano/Bozen, Italy</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="https://www1.in.tum.de/lehrstuhl_1/people/338-tobias-roehm">Tobias	Roehm</a>
+        <p>CQSE GmbH, Germany</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="https://www.cs.usask.ca/~croy/">Chanchal K. Roy</a>
+        <p>University of Saskatchewan, Canada</p>
+    </div>
+</div>
+<!-- 16° RIGA -->
+<div class="row">
+    <div class="col-md-4">
+        <a target="_blank" href="http://www.inf.unibz.it/~russo/">Barbara	Russo</a>
+        <p>Free University of Bolzano/Bozen, Italy</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="http://www.win.tue.nl/~aserebre/">Alexander Serebrenik</a>
+        <p>Eindhoven University of Technology, The Netherlands</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="http://das.encs.concordia.ca/members/emad-shihab/">Emad Shihab</a>
+        <p>Concordia University, Canada</p>
+    </div>
+</div>
+<!-- 17° RIGA -->
+<div class="row">
+    <div class="col-md-4">
+        <a target="_blank" href="http://ctreude.ca">Christoph Treude</a>
+        <p>University of Adelaide, Australia</p>
+    </div>
+    <div class="col-md-4">
+        <a target="_blank" href="http://homepages.dcc.ufmg.br/~mtov/">Marco Tulio	Valente</a>
+        <p>Universidade Federal de Minas Gerais, Brazil</p>
+    </div>
+    <div class="col-md-4">
+        <a>Jeroen van den Bos</a>
+        <p>Netherlands Forensic Institute, The Netherlands</p>
+    </div>
+</div>
+<!-- 18° RIGA -->
+<div class="row">
+    <div class="col-md-4">
+        <a target="_blank" href="http://my.ece.queensu.ca/People/Y-Zou/">Ying Zou</a>
+        <p>Queen's University, Canada</p>
+    </div>
+</div>
