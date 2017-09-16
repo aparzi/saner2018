9c63b33d49cd4fa9b87d0649f48f4e8f4ce45128
Angelo <angeloparziale94@gmail.com>
2017-09-16 12:52:33 +0200

    nuova pagina

---
+++ keynote.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>SANER 2018 - Campobasso, Italy</title>
+    <meta charset="utf-8">
+    <meta name="description" content="SANER 2018 - Campobasso, Italy">
+    <meta name="author" content="Angelo Parziale" />
+    <meta name="keywords" content="saner, html5, css3, campobasso, italy, molise" />
+    <!-- Bootstrap -->
+    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <!-- Icons/FontAwesome -->
+    <link rel="stylesheet" href="css/font-awesome.min.css" />
+    <!-- totop -->
+    <link rel="stylesheet" href="css/ui.totop.css" />
+    <!-- owl carusol -->
+    <link rel="stylesheet" href="css/owl.carousel.css" />
+    <!-- zetta menu -->
+    <link rel="stylesheet" href="css/zetta-menu.min.css" />
+    <!-- layerSlider -->
+    <link rel="stylesheet" href="css/layerslider.css" />
+    <!-- Animation -->
+    <link rel="stylesheet" href="css/animate.css" />
+    <!-- Customizable CSS -->
+    <link rel="stylesheet" href="css/style.css" />
+
+    <!-- FONTS -->
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="img/favicon/favicon-96x96.png">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" >
+    <!-- HTML5 elements and media queries Support for IE8 : HTML5 shim and Respond.js -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+<?php
+require_once ('./utils/headerTopBar.php');
+require_once ('./utils/navbar.php');
+?>
+
+<!-- Breadcrumbs -->
+<?php
+require_once ('./utils/breadcrumb.php');
+?>
+<!-- end Breadcrumbs -->
+
+<!-- what-we-do -->
+<section id="what-we-do" class="content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p style="text-align: justify;">
+                    We are honoured to announce not two but three eminent keynotes. Stay tuned for more details.
+                </p>
+            </div>
+        </div>
+        <div class="row padd">
+            <div class="col-md-2">
+                <img class="img-circle" style="max-width: 150px;" src="img/keynotes/JanBosch.png" alt="Jan Bosh" />
+            </div>
+            <div class="col-md-5 padd2">
+                <div class="team-date">
+                    <br />
+                    <h6><a href="http://www.janbosch.com" target="_blank">Jan Bosch</a>,<span style="font-size: 15px; font-weight: 200;">&nbspChalmers University of Technology (Sweden)</span></h6>
+                    <i>Towards a New Digital Business Operating System</i>
+                    <br><br>
+                    <div class="row">
+                        <div id="iconBosh" class="col-md-6 col-xs-12">
+                            <i title="Abstract" style="cursor: pointer; font-size: 20px;" data-toggle="modal" data-target="#abstractBosh" class="fa fa-file-text-o" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;
+                            <i title="Bio" style="cursor: pointer; font-size: 20px;" data-toggle="modal" data-target="#bioBosh" class="fa fa-user-o" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;
+                            <a target="_blank" href="http://www.janbosch.com/" style="color: #000; cursor: pointer; font-size: 20px;"><i title="Home" class="fa fa-home" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row padd">
+            <div class="col-md-2">
+                <img class="img-circle" style="max-width: 150px;" src="img/keynotes/Elmar-Jurgens.png" alt="Elmar Jurgens" />
+            </div>
+            <div class="col-md-4 padd2">
+                <div class="team-date">
+                    <br />
+                    <h6><a href="https://www.cqse.eu/en/team/elmar-juergens/" target="_blank">Elmar Jurgens</a>,<span style="font-size: 15px; font-weight: 200;">&nbspCQSE GmbH (Germany)</span></h6>
+                    <i>TBA</i>
+                    <br><br>
+                    <div class="row">
+                        <div id="iconElmar" class="col-md-6 col-xs-12">
+                            <i style="cursor: not-allowed; font-size: 20px;" data-toggle="modal" class="fa fa-file-text-o" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;
+                            <i style="cursor: not-allowed; font-size: 20px;" data-toggle="modal" class="fa fa-user-o" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;
+                            <a target="_blank" href="https://www.cqse.eu/en/team/elmar-juergens/" style="color: #000; cursor: pointer; font-size: 20px;"><i class="fa fa-home" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row padd">
+            <div class="col-md-2">
+                <img class="img-circle" style="max-width: 150px;" src="img/keynotes/peter_gromov.jpg" alt="Peter Gromov" />
+            </div>
+            <div class="col-md-4 padd2">
+                <div class="team-date">
+                    <br />
+                    <h6><a href="https://twitter.com/donnerpeter?lang=en" target="_blank">Peter Gromov</a>,<span style="font-size: 15px; font-weight: 200;">&nbspJetBrains (Germany)</span></h6>
+                    <i>TBA</i>
+                    <br><br>
+                    <div class="row">
+                        <div id="iconPeter" class="col-md-6 col-xs-12">
+                            <i style="cursor: not-allowed; font-size: 20px;" data-toggle="modal" class="fa fa-file-text-o" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;
+                            <i style="cursor: not-allowed; font-size: 20px;" data-toggle="modal" class="fa fa-user-o" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;
+                            <a target="_blank" href="https://twitter.com/donnerpeter?lang=en" style="color: #000; cursor: pointer; font-size: 20px;"><i class="fa fa-home" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- end of row -->
+</section>
+
+<!-- Modal Bio Bosh -->
+<div id="bioBosh" class="modal fade" style="z-index: 99999;" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Short bio of Jan Bosch</h4>
+            </div>
+            <div class="modal-body">
+                <p>
+                    Jan Bosch is professor of software engineering at Chalmers University Technology in Gothenburg, Sweden. He is director of the Software Center (<a href="http://www.software-center.se" target="_blank">www.software-center.se</a>), a strategic partner-funded collaboration between 10 large European companies (including Ericsson, Volvo Cars, Volvo Trucks, Saab Defense, Jeppesen (Boeing)  and  Siemens) and five universities focused on software engineering excellence. Earlier, he worked as Vice President Engineering Process at Intuit Inc where he also led Intuit's Open Innovation efforts and headed the central mobile technologies team. Before Intuit, he was head of the Software and Application Technologies Laboratory at Nokia Research Center, Finland. Prior to joining Nokia, he headed the software engineering research group at the University of Groningen, The Netherlands. He received a MSc degree from the University of Twente, The Netherlands, and a PhD degree from Lund University, Sweden. His research activities include evidence-based development, software architecture, innovation experiment systems, compositional software engineering, software ecosystems, software product families and software variability management. He is the author of several books including "Design and Use of Software Architectures: Adopting and Evolving a Product Line Approach" published by Pearson Education (Addison-Wesley & ACM Press) and “Speed, Data and Ecosystems: Excelling in a Software-Driven World” published by Taylor and Francis, editor of several books and volumes and author of a significant number of research articles. He is editor for Journal of Systems and Software as well as Science of Computer Programming, chaired several conferences as general and program chair, served on numerous program committees and organized countless workshops.<br>
+                    In the startup space, Jan is chairman of the board of Fidesmo in Stockholm,  Auqtus and, until recently, Remente, in Gothenburg, Sweden. He serves on the advisory board of Assia Inc. in Redwood City, CA, Peltarion AB in Stockholm and Burt AB in Gothenburg, Sweden. Jan also runs a boutique consulting firm, Boschonian AB, that offers its clients support around the implications of digitalization including the management of R&D and innovation. For more information see his website: <a target="_blank" href="http://www.janbosch.com">www.janbosch.com</a>.
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<!-- Modal Bio Bosh -->
+<div id="abstractBosh" class="modal fade" style="z-index: 99999;" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Towards a New Digital Business Operating System</h4>
+            </div>
+            <div class="modal-body">
+                <p>
+                    We are living in the most exciting time in the history of mankind. The last century has seen unprecedented improvements in the quality of the human condition and technology is at the heart of this progress. Now we are experiencing an even bigger leap as we move towards a new level of digitalization and automation. Ranging from self-driving cars to factories without workers to societal infrastructure, every sensor and actuator is becoming connected and new applications that enable new opportunities are appearing daily. The fuel of this emerging connected, software-driven reality is software and the key challenge is to continuously deliver value to customers. The future of software engineering in this context is centered around a new, emerging digital business operating system consisting of four dimensions: Speed, Data, Ecosystems and Empowerment. The focus on speed is concerned with the constantly increasing rate of deploying new software in the field. This continuous integration and deployment is no longer only the purview of internet companies but is also increasingly deployed in embedded systems. Second, data is concerned with the vast amounts of information collected from systems deployed in the field and the behavior of the users of these systems. Software businesses need to significantly improve their ability to exploit the value present in that data. Third, ecosystems are concerned with the transition in many companies from doing everything in-house to strategic use of innovation partners and commodity providing partners. Finally, we need new ways of organizing work in this new, digital age. The keynote discusses these four main developments but focuses on the continuous software engineering. Also, the keynote provides numerous examples from the Nordic and international industry and predicts the next steps that industry and academia need to engage in to remain competitive.
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!-- footer -->
+<?php require_once ('./utils/footer.php') ?>
+<!-- end footer -->
+
+<!-- jQuery -->
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.easing.min.js"></script>
+<script src="js/jquery.fitvids.js"></script>
+<script src="js/owl.carousel.js"></script>
+<script src="js/jquery.ui.totop.min.js"></script>
+<script src="js/jquery.countTo.js"></script>
+<script src="js/jquery.placeholder.js"></script>
+<script src="js/jquery.parallax-1.1.3.js"></script>
+<script src="js/greensock.js"></script>
+<script src="js/layerslider.transitions.js"></script>
+<script src="js/layerslider.kreaturamedia.jquery.js"></script>
+<script src="js/importantDates.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/main.js"></script>
+<script>
+    /* DOPO APERTURA DEL MODAL */
+    $("#bioBosh, #abstractBosh").on('shown.bs.modal', function(e){
+        $('li.zm-active').removeClass('zm-active')
+    });
+    /* DOPO LA CHIUSURA DEL MODAL */
+    $("#bioBosh, #abstractBosh").on('hidden.bs.modal', function(e){
+        $('#key').addClass('zm-active')
+    });
+</script>
+
+<script>
+    //Animation Wow.js
+    new WOW().init();
+</script>
+
+</body>
+</html>
--- restrack.php
+++ restrack.php
@@ -10,32 +10,32 @@
     <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
 
     <!-- Bootstrap Core CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../css/bootstrap.min.css" />
     <!-- Icons/FontAwesome -->
-    <link rel="stylesheet" href="css/font-awesome.min.css" />
+    <link rel="stylesheet" href="../css/font-awesome.min.css" />
     <!-- totop -->
-    <link rel="stylesheet" href="css/ui.totop.css" />
+    <link rel="stylesheet" href="../css/ui.totop.css" />
     <!-- owl carusol -->
-    <link rel="stylesheet" href="css/owl.carousel.css" />
+    <link rel="stylesheet" href="../css/owl.carousel.css" />
     <!-- zetta menu -->
-    <link rel="stylesheet" href="css/zetta-menu.min.css" />
+    <link rel="stylesheet" href="../css/zetta-menu.min.css" />
     <!-- layerSlider -->
-    <link rel="stylesheet" href="css/layerslider.css" />
+    <link rel="stylesheet" href="../css/layerslider.css" />
     <!-- Animation -->
-    <link rel="stylesheet" href="css/animate.css" />
+    <link rel="stylesheet" href="../css/animate.css" />
     <!-- Customizable CSS -->
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="../css/style.css" />
 
     <!-- FONTS -->
     <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
 
     <!-- Favicon -->
-    <link rel="shortcut icon" href="img/favicon/favicon-96x96.png">
+    <link rel="shortcut icon" href="../img/favicon/favicon-96x96.png">
 
     <!-- HTML5 elements and media queries Support for IE8 : HTML5 shim and Respond.js -->
     <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
+    <script src="../js/html5shiv.js"></script>
     <script src="js/respond.min.js"></script>
     <![endif]-->
 
@@ -44,13 +44,13 @@
 <body>
 
 <?php
-require_once ('./utils/headerTopBar.php');
-require_once ('./utils/navbar.php');
+require_once('../utils/headerTopBar.php');
+require_once('../utils/navbar.php');
 ?>
 
 <!-- Breadcrumbs -->
 <?php
-require_once ('./utils/breadcrumb.php');
+require_once('../utils/breadcrumb.php');
 ?>
 <!-- end Breadcrumbs -->
 
@@ -124,7 +124,7 @@ require_once ('./utils/breadcrumb.php');
             </div>
         </div>
         <?php
-        $content = file_get_contents('config/production.json');
+        $content = file_get_contents('../config/production.json');
         $json = json_decode($content, true);
         ?>
         <div class="row"> <?php
@@ -132,7 +132,7 @@ require_once ('./utils/breadcrumb.php');
                 ?>
                 <div class="col-md-4">
                     <?php if (!is_null($value['website'])) { ?>
-                        <a href="<?php echo $value['website'] ?>" target="_blank"><?php echo $value['name'] ?></a>
+                        <a href="<?php echo $value['website'] ?>" target="_blank"><?php echo $value['name'] ?></a><?php if ($value['name'] == 'Massimiliano Di Penta' || $value['name'] == 'David C. Shepherd') { echo ' <span style="font-size: 18px;">(co-Chair)</span>';} ?>
                     <?php } else { ?>
                         <a><?php echo $value['name'] ?></a>
                     <?php } ?>
@@ -146,25 +146,25 @@ require_once ('./utils/breadcrumb.php');
 </section>
 
 <!-- footer -->
-<?php require_once ('./utils/footer.php') ?>
+<?php require_once('../utils/footer.php') ?>
 <!-- end footer -->
 
 <!-- jQuery -->
-<script src="js/jquery-1.11.1.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/jquery.easing.min.js"></script>
-<script src="js/jquery.fitvids.js"></script>
-<script src="js/owl.carousel.js"></script>
-<script src="js/jquery.ui.totop.min.js"></script>
-<script src="js/jquery.countTo.js"></script>
-<script src="js/jquery.placeholder.js"></script>
-<script src="js/jquery.parallax-1.1.3.js"></script>
-<script src="js/greensock.js"></script>
-<script src="js/layerslider.transitions.js"></script>
-<script src="js/layerslider.kreaturamedia.jquery.js"></script>
-<script src="js/importantDates.js"></script>
-<script src="js/wow.min.js"></script>
-<script src="js/main.js"></script>
+<script src="../js/jquery-1.11.1.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<script src="../js/jquery.easing.min.js"></script>
+<script src="../js/jquery.fitvids.js"></script>
+<script src="../js/owl.carousel.js"></script>
+<script src="../js/jquery.ui.totop.min.js"></script>
+<script src="../js/jquery.countTo.js"></script>
+<script src="../js/jquery.placeholder.js"></script>
+<script src="../js/jquery.parallax-1.1.3.js"></script>
+<script src="../js/greensock.js"></script>
+<script src="../js/layerslider.transitions.js"></script>
+<script src="../js/layerslider.kreaturamedia.jquery.js"></script>
+<script src="../js/importantDates.js"></script>
+<script src="../js/wow.min.js"></script>
+<script src="../js/main.js"></script>
 <script>
     jQuery("#layerslider").layerSlider({
         responsive: false,
