f2d9e7cdb2a768dffe4a77be71ab0a1d8cb85586
Angelo <angeloparziale94@gmail.com>
2017-10-03 23:32:41 +0200

    aggiunto snippet google analytics

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-58052891-3"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments)};
+    gtag('js', new Date());
+
+    gtag('config', 'UA-58052891-3');
+</script>
+<!-- END ANALYTICS -->
 <!DOCTYPE html>
 <html lang="en">
     <head>
