9234ef5170e8e40ce3cf95a6ba5b0f4e9aadcfea
Simone Scalabrino <s.scalabrino9@gmail.com>
2018-03-17 11:52:47 +0100

    Changed program of Program Analysis

--- sessions/program_analysis.php
+++ sessions/program_analysis.php
@@ -1,6 +1,6 @@
 <details>
     <summary>Program Analysis</summary>
-    <div class="chair">Session Chair: Jim Cordy</div>
+    <div class="chair">Session Chair: Mike Godfrey</div>
     <p>
         <ul>
             <li>
@@ -8,14 +8,6 @@
                     <?= paper() ?> 
                     <span class="title">Context Is King: The Developer Perspective on the Usage of Static Analysis Tools</span>
                     <span class="authors">Carmine Vassallo, Sebastiano Panichella, Fabio Palomba, Sebastian Proksch, Andy Zaidman and Harald Gall</span>
-                </div>
-            </li>
-            
-            <li>
-                <div class="program_paper">
-                    <?= paper() ?> 
-                    <span class="title">SQL-aware Fault Localization in PHP-based Web Applications</span>
-                    <span class="authors">Hung Nguyen, Hoan Nguyen and Tien Nguyen</span>
                 </div>
             </li>
             
