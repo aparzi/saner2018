a98764aa85854c843e5ae9dad486dda694702596
aparzi <angeloparziale94@gmail.com>
2018-03-20 14:30:11 +0100

    fix

--- resources/download.php
+++ resources/download.php
@@ -6,7 +6,7 @@ $pass = "xo8shaiPoh";
 #Se la variabile PHP_AUTH_USER non è settata, il browser mostra la finestra di login
 if (!isset($_SERVER['PHP_AUTH_USER'])) {
     header("WWW-Authenticate: Basic realm=" . " Effettua il login");
-    Header("HTTP/1.0 401 Unauthorized");
+    header("HTTP/1.0 401 Unauthorized");
     exit;
 } elseif ($_SERVER['PHP_AUTH_USER'] == $user && $_SERVER['PHP_AUTH_PW'] == $pass) {
     # in caso di login corretta
